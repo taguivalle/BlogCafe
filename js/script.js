bf12badfc4fb42cd2ef4a4ae4b57ebd4e2390824
Hector gustavo vallejo espinosa <taguivalle140965@gmail.com>
2024-01-18 19:02:35 -0500

    I did that lesson one hundred forty four, call Select Elements with querySelector

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// querySelector.
+const heading = document.querySelector('.header__texto h2'); // cero (0) o un (1) elemento
+heading.classList = 'Una-nueva-clase';
+heading.textContent = 'New heading';
+console.log(heading);
+// querySelectorAll.
+// getElementById.
